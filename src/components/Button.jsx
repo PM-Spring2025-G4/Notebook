49baaea04408065c9de0d67fcaf62463a4ec03c5
Aleksi Kesälä <alkesala@gmail.com>
2025-01-28 12:18:31 +0200

    fix: tailwind v4 upgraded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({ label, onClick }) => (
   <button
     onClick={onClick}
-    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+    className="px-4 py-2 text-white bg-blue-500 rounded-sm hover:bg-blue-700"
   >
     {label}
   </button>
