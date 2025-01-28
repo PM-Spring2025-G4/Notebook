007babe660f31b92daac277ddaf65e3b8e7becae
Aleksi Kesälä <alkesala@gmail.com>
2025-01-28 12:14:48 +0200

    bugfix: fixed bugs caused by removing ts

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,15 @@
 
 // USE export const
 
-interface ButtonProps {
-  label: string
-  onClick: () => void
-}
+// The interface declaration is not needed in a JavaScript file.
+// You can remove the following code:
 
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => (
+// interface ButtonProps {
+//   label: string
+//   onClick: () => void
+// }
+
+export const Button = ({ label, onClick }) => (
   <button
     onClick={onClick}
     className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import { Button } from "../components/Button"
 
-export const Home: React.FC = () => {
+export const Home = () => {
   const handleClick = () => alert("Buy me a coffee!")
 
   return (
     <div className="p-4 text-center">
-      <h1 className="text-2xl font-bold text-blue-700">
-        THIS IS OUR NOTEBOOK
-      </h1>
+      <h1 className="text-2xl font-bold text-blue-700">THIS IS OUR NOTEBOOK</h1>
       <Button label="Click Me" onClick={handleClick} />
     </div>
   )
